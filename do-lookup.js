85524f52be763293231cbdfae3bc6cd6357927c5
frt4 <frt4@students.uwf.edu>
2023-01-10 11:50:18 -0600

    Run requests on input

--- do-lookup.js
+++ do-lookup.js
@@ -1,3 +1,7 @@
+var axios = require('axios');
+const shodan = 'https://internetdb.shodan.io/';
+const successStatusCode = 200;
+
 /**
  * The `doLookup` method should query the Shodan InternetDB for each IPv4 address passed into the `entities`
  * parameter.  You can try your method out by running the command `npm run search` in the root of this project.
@@ -11,6 +15,48 @@ async function doLookup(entities) {
     // Add additional logic here that will query the Shodan InternetDB API
     // and add the results to the `lookupResults` array.
     // Please see the README.md for full instructions
+
+    let requests = [];
+    // build out the request objects
+    for (let i = 0; i < entities.length; i++) {
+        // make sure the value exists
+        if (entities[i].value) {
+            const fullUrl = shodan + entities[i].value;
+            requests.push(axios.get(fullUrl, {
+                validateStatus: function(status) {
+                    return status; // return all status codes as they will just be logged as data null
+                }
+            }));
+        }
+        
+    }
+
+    await axios.all(requests)
+    .then(await function(results) {
+        results.forEach(result => {
+            if (result.status === successStatusCode) {
+                const index = entities.findIndex((x) => x.value === result.request.path.substring(1));
+                if (index !== -1) {
+                    lookupResults.push({
+                        entity: entities[index],
+                        data: result.data
+                    });
+                }
+            }
+            else {
+                const index = entities.findIndex((x) => x.value === result.request.path.substring(1));
+                if (index !== -1) {
+                    lookupResults.push({
+                        entity: entities[index],
+                        data: null
+                    });
+                }
+            }
+        });
+    }).catch(error => {
+        console.log(error);
+    });
+
     return lookupResults;
 }
 
